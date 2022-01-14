9fc9fbaa51a11d0bcae1af460c644066e7b0c192
congvo0412 <0306191302@caothang.edu.vn>
2022-01-14 23:50:37 +0700

    addAll

---
+++ lib/screens/account.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:doan_ltdd/main.dart';
+import 'package:doan_ltdd/screens/home.dart';
+import 'package:doan_ltdd/screens/category.dart';
+import 'package:doan_ltdd/screens/account.dart';
+import 'package:doan_ltdd/screens/notification.dart';
+import 'package:doan_ltdd/screens/cart.dart';
+import 'package:doan_ltdd/screens/login.dart';
+
+class AccountPage extends StatelessWidget {
+  const AccountPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Colors.deepOrange,
+        title: Center(child: Text("Tài khoản của tôi")),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Icon(Icons.settings),
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Hồ sơ",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ))),
+              Divider(),
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Đơn hàng",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ))),
+              Divider(),
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Ví của tôi",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ))),
+              Divider(),
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Hỗ trợ",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ))),
+              Divider(),
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Địa chỉ",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ))),
+              Divider(),
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Voucher của tôi",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ))),
+              Divider(),
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Cài đặt",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ))),
+              Divider(),
+              Container(
+                  alignment: Alignment.topLeft,
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => ShopingTab()));
+                      },
+                      child: Text(
+                        "Đăng xuất",
+                        style: TextStyle(fontSize: 20, color: Colors.black),
+                      ))),
+              Divider(),
+            ],
+          )
+        ],
+      ),
+    ); 
+  }
+}
